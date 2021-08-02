1efeec0d95552fee2914f82750d619b57ef4aa9d
dima-starkov <62872531+dima-starkov@users.noreply.github.com>
2021-08-02 21:30:07 +0500

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Дмитрий Старков on 02.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
