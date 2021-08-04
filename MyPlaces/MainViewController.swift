6e57a4295cab6ce631987c1b45ade13f2c9b2624
dima-starkov <62872531+dima-starkov@users.noreply.github.com>
2021-08-04 15:58:06 +0500

    Add icon Launch screen

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -30,7 +30,15 @@ class MainViewController: UITableViewController {
         let text = restaurantNames[indexPath.row]
         cell.textLabel?.text = text
         cell.imageView?.image = UIImage(named: text)
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
     
     // MARK: - Navigation
