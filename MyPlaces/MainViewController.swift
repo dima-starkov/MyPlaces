92b2bf476eaa245aa441ca058152b3c940db7be4
dima-starkov <62872531+dima-starkov@users.noreply.github.com>
2021-08-04 16:32:50 +0500

    Custom TableViewCell

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -26,12 +26,12 @@ class MainViewController: UITableViewController {
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
         let text = restaurantNames[indexPath.row]
-        cell.textLabel?.text = text
-        cell.imageView?.image = UIImage(named: text)
-        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
-        cell.imageView?.clipsToBounds = true
+        cell.nameLabel.text = text
+        cell.imageOfPlace.image = UIImage(named: text)
+        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
+        cell.imageOfPlace.clipsToBounds = true
         return cell
     }
     
